ef295e09f27d7f94da46a367a024dffc56739689
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2019-01-13 15:59:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,13 +33,22 @@ bot.on('message', message => {
     }
     if (message.content === prefix + "trad" ){ 
         const embed = new Discord.RichEmbed()
-    .setTitle("Avancement de la traduction:")
-    .setColor(0x0086AE)
-    .setDescription("fait : :white_check_mark:\na verifier : :ballot_box_with_check: \nnon fait : :x:\nprobleme : :no_entry:")
-    .setFooter("(metre ici le pourcentage)")
-    .addField("1-20",
-      "NOOO")
-      message.channel.send({embed});
+        .setTitle("This is your title, it can hold 256 characters")
+        .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+        .setColor(0x00AE86)
+        .setDescription("This is the main body of text, it can hold 2048 characters.")
+        .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+        .setImage("http://i.imgur.com/yVpymuV.png")
+        .setThumbnail("http://i.imgur.com/p2qNFag.png")
+        .setTimestamp()
+        .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+        .addField("This is a field title, it can hold 256 characters",
+          "This is a field value, it can hold 1024 characters.")
+        .addField("Inline Field", "They can also be inline.", true)
+        .addBlankField(true)
+        .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+       
+        message.channel.send({embed});
     }
 }
 }     
