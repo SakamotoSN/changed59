88b8d74e6f58c964a1276eea9fa189610eac54d3
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2019-02-16 14:34:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,24 +32,13 @@ bot.on('message', message => {
         message.delete();
     }
 }
-if(message.content.startsWith(">journal") ){
+if (message.content.startsWith(prefix + "journal")){
     var embed = new Discord.RichEmbed()
-    .setTitle("Iphone X Demon")
-    .setColor(0xD4FE00)
-    .setDescription("***Journal des Démons du mois de Février***")
-    .addField("Union Upsilon : Extermination par les démons ?",
-              "La semaine dernière , L'Union Upsilon à du faire face au 3 chefs démoniaque !\nLeur chef , Guten , nous explique leur défaite :\n*Nous étions environ 50 000 face à eux ! Notre Union étant l'une des plus puissante , pensions gagner facilement quand les 3 grands chefs démoniaque sont apparus et ont exterminés les*")
-    console.log("journal fait ^^")
+    .addField("**Transition**", "Voici les Commandes Disponibles !")
+    .addField("```actions```", "```a_calin @mention\na_tue @mention\na_claque @mention\na_embrasse @mention```")
+    .setColor(0x66c2ff)
+    .addField("```Admin```", "```a_warn @mention raison\na_dit message```")
+    message.channel.send(embed)
 }
-if(message.content.startsWith(">journal_2") ){
-    var embed = new Discord.RichEmbed()
-    .setTitle("Iphone X Demon")
-    .setColor(0xD4FE00)
-    .setDescription("***Journal des Démons du mois de Février***")
-    .addField("...",
-    "*3 quarts de mes hommes ... Surtout que les chefs démoniaques n'étaient normalement que rang D ! Je ne sais pas ce qui leur est arrivé , mais ils sont devenus beaucoup plus fort qu'avant , et*")
-.addField("...",
-    "*c'est d'ailleurs pour cette raison que ces 3 là sont passés en rang B ! Même leur pion était plus fort et intelligent qu'avant , ils ont changés quelques chose mais quoi ? Nous envoyons une Union plus\nforte enquêter sur le sujet !*\nAlors est-ce que les démons ont pris la relève ? Va t'on reperdre notre territoire et se faire exterminer une bonne fois pour toute ? A suivre ...")
-    console.log("journal page 2 fait ^^")}
 }  
 )
